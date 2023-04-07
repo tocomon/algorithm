3899eb34edafe8cfb6b572baf1a2d2f38bdde406
tocomon <128657695+tocomon@users.noreply.github.com>
2023-04-07 11:14:23 +0900

    init

---
+++ week01/10869.py
@@ -0,0 +1,8 @@
+import sys  
+a = list(map(int, sys.stdin.readline().split()))
+
+print(a[0]+a[1])
+print(a[0]-a[1])
+print(a[0]*a[1])
+print(int(a[0]/a[1]))
+print(a[0]%a[1])
---
+++ week01/2588.py
@@ -0,0 +1,6 @@
+import sys  
+n = input()  
+a = [sys.stdin.readline() for i in range(n)]  
+# a = ["1 2 3", "4 5 6"]
+
+
