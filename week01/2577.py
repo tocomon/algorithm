b7781d3f172c5dccd533db79b2a61a05b74f902e
tocomon <128657695+tocomon@users.noreply.github.com>
2023-04-07 22:17:34 +0900

    2577

--- week01/2577.py
+++ week01/2577.py
@@ -1,3 +1,17 @@
 import sys  
-n = input()  
-a = [sys.stdin.readline() for i in range(n)]  
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+c = int(sys.stdin.readline())
+
+n =a*b*c
+
+for i in range(10):
+    count=0
+    for j in str(n):
+        if i == int(j):
+            count+=1
+
+    print(count)
+
+
