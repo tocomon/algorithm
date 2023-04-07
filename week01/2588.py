0fc75a7db449849ca58f8e93b9341263ea2a588b
tocomon <128657695+tocomon@users.noreply.github.com>
2023-04-07 11:29:24 +0900

    2588

--- week01/2588.py
+++ week01/2588.py
@@ -1,6 +1,14 @@
-import sys  
-n = input()  
-a = [sys.stdin.readline() for i in range(n)]  
-# a = ["1 2 3", "4 5 6"]
+import sys
 
+num1 = int(sys.stdin.readline().strip())
+num2 = int(sys.stdin.readline().strip())
 
+num3 = num1 * (num2 % 10)  # 3번째 자리
+num4 = num1 * ((num2 // 10) % 10)  # 4번째 자리
+num5 = num1 * (num2 // 100)  # 5번째 자리
+num6 = num1 * num2  # 6번째 자리
+
+print(num3)
+print(num4)
+print(num5)
+print(num6)
