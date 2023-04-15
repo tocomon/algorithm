6c6dd53981762442e486c384aa54e69878e3fc31
tocomon <128657695+tocomon@users.noreply.github.com>
2023-04-15 15:56:18 +0900

    calculator

--- week02/8983.py
+++ week02/8983.py
@@ -1,30 +1,33 @@
 import sys
 
-def binary_search(left, right, target):
-    while left <= right:
-        mid = (left + right) // 2
-        if animals[mid][0] <= target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return right
+def count_animals(x_pos, y_pos, hunters, L):
+    count = 0
+    for x, y in zip(x_pos, y_pos):
+        left = 0
+        right = len(hunters) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if abs(hunters[mid] - x) + y <= L:
+                count += 1
+                break
+            elif hunters[mid] > x:
+                if mid == left:
+                    break
+                right = mid - 1
+            else:
+                if mid == right:
+                    break
+                left = mid + 1
+    return count
 
-m, n, l = map(int, sys.stdin.readline().split())
-guns = list(map(int, sys.stdin.readline().split()))
-animals = []
-for _ in range(n):
-    x, y = map(int, sys.stdin.readline().split())
-    if y <= l:
-        animals.append((x, y))
-animals.sort(key=lambda x: x[1])
-
-answer = 0
-for animal in animals:
-    left = binary_search(0, m - 1, animal[0] - l + guns[0])
-    right = binary_search(0, m - 1, animal[0] + l + guns[-1])
-    for i in range(left, right + 1):
-        if abs(guns[i] - animal[0]) + animal[1] <= l:
-            answer += 1
-            break
-
-print(answer)
+M, N, L = map(int, input().split())
+hunters = sorted(map(int, input().split()))
+x_pos = []
+y_pos = []
+for i in range(N):
+    x, y = map(int, input().split())
+    if y > L:
+        continue
+    x_pos.append(x)
+    y_pos.append(y)
+print(count_animals(x_pos, y_pos, hunters, L))
