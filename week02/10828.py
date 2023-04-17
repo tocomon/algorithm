eff29cada2ff0581ad8f1cc1da5b3ef995ae502a
tocomon <128657695+tocomon@users.noreply.github.com>
2023-04-17 22:31:05 +0900

    stack

---
+++ week02/10828.py
@@ -0,0 +1,28 @@
+import sys
+
+stack = []
+
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    command = sys.stdin.readline().split()
+    
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'pop':
+        if not stack:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if not stack:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
