481c18d16248c99b1d20d5bdf162985c1d4a420a
tocomon <128657695+tocomon@users.noreply.github.com>
2023-04-20 23:50:55 +0900

    graph

---
+++ week03/5639.py
@@ -0,0 +1,24 @@
+import sys
+input=sys.stdin.readline
+
+a=[]
+
+while True:
+    try:
+        a.append(int(input()))
+    except:
+        break
+
+def post(start, end):
+    if start > end:
+        return
+    mid = end + 1
+    for i in range(start + 1, end + 1):
+        if a[i] > a[start]:
+            mid = i
+            break
+    post(start + 1, mid - 1) #왼쪽 트리
+    post(mid, end) #오른쪽 트리
+    print(a[start]) #루트 노드
+
+post(0, len(a) - 1)
