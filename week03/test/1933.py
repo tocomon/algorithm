56d76b3cbd8389e1ef1bcfa69143cd9ec031758d
tocomon <128657695+tocomon@users.noreply.github.com>
2023-04-20 23:51:02 +0900

    graph

--- week03/test/1933.py
+++ week03/test/1933.py
@@ -1,33 +1,110 @@
 import sys
-from collections import defaultdict as dd
-from bisect import bisect_left as bl
+input = sys.stdin.readline
+
+
+def skyline(buildings):
+    
+    if not buildings:
+        return []
+    
+    #각 분할이 끝나면 해당 좌표로 변환 
+    if len(buildings) == 1:
+        return [[buildings[0][0], buildings[0][1]], [buildings[0][2], 0]]
+    
+    #분할 과정    
+    left = skyline(buildings[:len(buildings) // 2])
+    right = skyline(buildings[len(buildings) // 2:])
+    
+    return merge(left, right)
+
+
+
+#각각 나눠진 빌딩을 합치는 과정 
+def merge(left, right):
+    h1, h2 = 0, 0
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        
+        if left[i][0] < right[j][0]:
+            h1 = left[i][1]
+            corner = left[i][0]
+            i += 1
+            
+        elif right[j][0] < left[i][0]:
+            h2 = right[j][1]
+            corner = right[j][0]
+            j += 1
+            
+        else:
+            h1 = left[i][1]
+            h2 = right[j][1]
+            corner = right[j][0]
+            i += 1
+            j += 1
+            
+        if is_valid(result, max(h1,h2)):
+            result.append([corner, max(h1, h2)])
+    result.extend(right[j:])
+    result.extend(left[i:])
+    return result
+
+
+def is_valid(result, new_height):
+    return not result or result[-1][1] != new_height
+
+
+
+n = int(input())
+arr = [list(map(int, input().split())) for i in range(n)]
+
+for i in skyline(arr):
+    print(*i, end = ' ')
+
+
+
+"""
+import sys
+from collections import defaultdict
+from bisect import bisect_left #이진탐색
 
 active = [0] # always sorted
 
-
-def apply(iterable):
+def apply(iterable): #
     for a, i in iterable:
-        idx = bl(active, a)
-        active[idx:idx+i] = [] if i else [a]
-
+        idx = bisect_left(active, a) # active에서 a가 들어갈 index
+        active[idx:idx+i] = [] if i else [a] # idx 부터 i 만틈 비우거나, [a] 값으로 대체
 
 input = sys.stdin.readline
 n = int(input())
 
-start = dd(list)
+start = defaultdict(list) # {1: [(11, 0)], 5: [(11, 1)]} 형태
 for i in range(n):
     l, h, r = map(int, input().split())
     start[l].append((h, 0))
     start[r].append((h, 1))
+# print("start: ", end='')
+# print(start)
 
-s = sorted(start.keys())
-m = 0
+s = sorted(start.keys()) # s: [1, 2, 3, 5, 7, 9, 12, 14, 16, 19, 22, 23, 24, 25, 28, 29]
+# print("s: ", end='')
+# print(s)
+
+m = 0 # 높이 저장
 write = []
 
-for i in s:
-    apply(start[i])
-    if m != active[-1]:
+for i in s: 
+    apply(start[i]) # 높이 체크
+    # print("active " + str(i) + ": ", end='')
+    # print(active)
+    if m != active[-1]: # 높이 변화 체크
         m = active[-1]
-        write.extend([i, m])
+        # print("active2: ", end='')
+        # print(active)
+        write.extend([i, m]) # 높이 변할 경우 출력
+        # print("write: ", end='')
+        # print(write)
+
 
 print(*write)
+"""
--- week03/test/5904.py
+++ week03/test/5904.py
@@ -1,14 +1,23 @@
-N = int(input())
-
-def moo(acc, cur, N):
-    prev = (acc-cur)//2
-    if N <= prev: return moo(prev, cur-1, N)
-    elif N > prev+cur: return moo(prev, cur-1, N-prev-cur)
-    else: return "o" if N-prev-1 else "m"
-
-acc, n = 3, 0
-while N > acc:
-    n += 1
-    acc = acc*2+n+3
-
-print(moo(acc, n+3, N))
+import sys
+s0 = ['m', 'o', 'o']
+def sol(n, k, l):                # n: Moo 수열의 n번째 글자(찾고자 하는 글자), # k: 차수, # l: 이전 차수의 길이
+    # new_l: 새로운 차수의 길이 # 점화식 l(k) = l(k-1) + (k+3) + l(k-1)
+    new_l = 2*l + k + 3
+    if n <= 3:                   # n이 3 이하일 경우 바로 출력
+        print(s0[n-1])
+        exit(0)
+    if new_l < n:                # new_l이 n보다 작을 경우
+        sol(n, k+1, new_l)       # k 차수를 하나씩 늘려준다. new_l이 n을 담을 수 있을 때까지
+    else:
+        # n은 l보다 무조건 크기 때문에 생각하지 않는다. 원래 3파트로 나눠서 봐야하는데 2파트만 보면 된다.
+        if n > l and n <= l+k+3:
+            if n-l != 1:         # n-l = 1일때만 'm'이 있고 나머지는 'o'로 채워진다.
+                print('o')
+            else:
+                print('m')
+            exit(0)
+        else:                    # 1+k+3 바깥에 있는 경우
+            # [n - (1+k+3)]을 진행해서 다시 첫번째 part로 돌아온 다음 처음부터 재귀를 돌리기 시작한다.
+            sol(n-(l+k+3), 1, 3)
+n = int(sys.stdin.readline())
+sol(n, 1, 3)
