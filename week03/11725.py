b36864eb09ea094bc5aa3cfa65fb8162aed5550c
tocomon <128657695+tocomon@users.noreply.github.com>
2023-04-23 23:46:25 +0900

    dfs

--- week03/11725.py
+++ week03/11725.py
@@ -1,67 +1,61 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
 
 def dfs(node, parent, depth, graph, parent_list):
-    parent_list[node] = parent
+    parent_list[node] = parent      
     for child in graph[node]:
         if child != parent:
             dfs(child, node, depth+1, graph, parent_list)
+
+def bfs(start, graph, parent_list):
+    queue = deque([start])
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            parent_list[i] = v
+            queue.append(i)
 
 n = int(input())
 graph = [[] for _ in range(n+1)]
 parent_list = [0] * (n+1)
 
 for i in range(n-1):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    try:
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    except:
+        break
 
 dfs(1, 0, 0, graph, parent_list)
-print(graph)
-print(parent_list)
-for i in range(2, n+1):
-    print(parent_list[i])
+
+for i in parent_list[2:]:
+    print(i)
 
 
-
-# # 틀렸습니다
 # import sys
-# from collections import deque
 # sys.setrecursionlimit(10**8)
 # input = sys.stdin.readline
 
-# def dfs(start):
-#     visited[start] = True      
-#     for i in graph[start]:
-#         if not visited[i]:
-#             dfs(i)
-
-# def bfs(start):
-#     queue = deque([start])
-#     visited[start] = True
-#     while queue:
-#         v = queue.popleft()
-#         for i in graph[v]:
-#             if not visited[i]:
-#                 visited[i] = True
-#                 queue.append(i)
+# def dfs(node, parent, depth, graph, parent_list):
+#     parent_list[node] = parent
+#     for child in graph[node]:
+#         if child != parent:
+#             dfs(child, node, depth+1, graph, parent_list)
 
 # n = int(input())
 # graph = [[] for _ in range(n+1)]
+# parent_list = [0] * (n+1)
 
 # for i in range(n-1):
-#     try:
-#         a, b = map(int, input().split())
-#         graph[a].append(b)
-#         graph[b].append(a)
-#     except:
-#         break
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
 
-# # 방문 여부 체크
-# visited = [False] * (n + 1)
-
-# dfs(1)
-
-# for i in graph[2:]:
-#     print(i[0])
+# dfs(1, 0, 0, graph, parent_list)
+# print(graph)
+# print(parent_list)
+# for i in range(2, n+1):
+#     print(parent_list[i])
---
+++ week03/1707.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+sys.setrecursionlimit(10**8)
+input = sys.stdin.readline
+
+def dfs(start):
+    global isValid
+    visited[start] = True 
+    for i in graph[start]:
+        if not visited[i]:
+            check[i] = (check[start]+1)%2
+            dfs(i)
+        elif check[start] == check[i]:
+            isValid =False
+
+n = int(input())
+
+for i in range(n):
+    isValid = True
+    v, e= map(int, input().split())
+    visited = [False] * (v + 1)
+    graph = [[] for _ in range(v+1)]
+    check = [0] * (v+1)
+    for j in range(e):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    for i in range(1, v+1):
+        if isValid:
+            dfs(i)
+        else:
+            break
+    if isValid:
+        print("YES")
+    else:
+        print("NO")
+
+
